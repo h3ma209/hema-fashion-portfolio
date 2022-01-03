bb308e22ea5cec2ed91b404100f4f82655f67c96
hema omer <32086055+h3ma209@users.noreply.github.com>
2022-01-03 20:02:14 +0300

    added loader

--- components/Loader.vue
+++ components/Loader.vue
@@ -22,7 +22,7 @@ export default {
   mounted() {
     this.$nextTick(() => {
       this.$nuxt.$loading.start();
-      setTimeout(() => this.$nuxt.$loading.finish(), 500);
+      setTimeout(() => this.$nuxt.$loading.finish(), 5000);
     });
   },
 };
